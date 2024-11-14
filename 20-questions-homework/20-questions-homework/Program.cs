13de0a84f64e4e27bdd8a4eea48ffdf2ef0dc5eb
mksigrest <mksigrest@gmail.com>
2024-11-14 08:41:16 -0500

    Update Program.cs

--- 20-questions-homework/20-questions-homework/Program.cs
+++ 20-questions-homework/20-questions-homework/Program.cs
@@ -5,12 +5,12 @@
         static void Main(string[] args)
         {
             /*
-             * Designing Node Tree Class: Meakalia
-             * Building the question tree: Max
-             * Traversal on input: Tommy
-             * Multiple rounds: Shawn
-             * Learning: Presley
-             * Saving: Shawn
+             * Designing Node Tree Class: Meakalia - Sat Have something - finish Sun - ask for help if you can't finish the work
+             * Building the question tree: Max - Sun have finished - ask for help if you can't finish the work
+             * Traversal on input: Tommy - Tue have work done - Wed have finished ask for help if you can't finish the work
+             * Multiple rounds: Shawn - Thur have finished - ask for help if you can't finish the work
+             * Learning: Presley - fri have something - sat Finished - ask for help if you can't finish the work
+             * Saving: Shawn - Finish on Sat - Hopefully all code is done on Sat, ask questions if we need help
             */
 
 
