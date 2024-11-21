2623523b2529cf3be1d2650ff717e15047d8fe78
meakaliaG <124390595+meakaliaG@users.noreply.github.com>
2024-11-21 08:42:00 -0500

    tree_node_class
    TreeNode class created with properties for individual data, a yes node and a no node

--- tree-node-class/tree-node-class/Program.cs
+++ tree-node-class/tree-node-class/Program.cs
@@ -2,6 +2,19 @@
 {
     internal class Program
     {
+        //tree_node_class
+
+    class TreeNode
+        {
+            public string Data { get; set; }
+            public TreeNode YesChild { get; set; }
+            public treeNode NoChild { get; set; }
+
+            public TreeNode(string data)
+            {
+                Data = data;
+            }
+
         static void Main(string[] args)
         {
             
