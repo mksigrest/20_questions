1646776e8f04a0be4e8d109367bfb28d31d29e39
mksigrest <mksigrest@gmail.com>
2024-11-21 09:02:43 -0500

    updated roles
    updated the roles to include extra jobs for Nick

--- 20-questions-homework/20-questions-homework/Program.cs
+++ 20-questions-homework/20-questions-homework/Program.cs
@@ -5,11 +5,11 @@
         static void Main(string[] args)
         {
             /*
-             * Designing Node Tree Class: Meakalia - Sat Have something - finish Sun - ask for help if you can't finish the work
-             * Building the question tree: Max - Sun have finished - ask for help if you can't finish the work
-             * Traversal on input: Tommy - Tue have work done - Wed have finished ask for help if you can't finish the work
-             * Multiple rounds: Shawn - Thur have finished - ask for help if you can't finish the work
-             * Learning: Presley - fri have something - sat Finished - ask for help if you can't finish the work
+             * Designing Node Tree Class & Testing the code: Meakalia - Sat Have something - finish Sun - ask for help if you can't finish the work
+             * Building the question tree & Error Handling: Max - Sun have finished - ask for help if you can't finish the work
+             * Traversal on input & Comments: Tommy - Tue have work done - Wed have finished ask for help if you can't finish the work
+             * Multiple rounds & Error Handling: Shawn - Thur have finished - ask for help if you can't finish the work
+             * Learning & Testing the Code: Presley - fri have something - sat Finished - ask for help if you can't finish the work
              * Saving: Shawn - Finish on Sat - Hopefully all code is done on Sat, ask questions if we need help
             */
 
